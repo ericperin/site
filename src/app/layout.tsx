8844f11a66f8d7aca8363299e867985437f10b5f
Eric Perin <eric.perin@outlook.com>
2024-03-24 18:46:01 -0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-E2N3RZJSK1" />
     </html>
   );
 }
